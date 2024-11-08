1b3818613ac1a23547daef68cd124c1d0de4dca2
cmastril <90735113+cmastril@users.noreply.github.com>
2024-11-08 15:03:22 -0500

    v3
    v3

---
+++ Javascript/testscript.js
@@ -0,0 +1,11 @@
+function loadFriendActivityPage() {
+    window.location.href = "page_friend_activity.html"
+}
+
+function loadEntrySubmissionPage() {
+    window.location.href = "page_entry_submission.html"
+}
+
+function loadProfilePage() {
+    window.location.href = "page_profile.html"
+}
